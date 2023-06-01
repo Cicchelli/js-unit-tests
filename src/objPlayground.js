1e65db0717b42e4e5559c9721c89a0283f555b36
Stephan <cicchelli.stephan@gmail.com>
2023-06-01 16:08:52 -0300

    Req objPlayground

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,20 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  const sum = number1 + number2;
+  const mult = number1 * number2;
+  const div = Math.floor(number1 / number2);
+  const sub = number1 - number2;
+  const Calc = {
+    sum,
+    mult,
+    div,
+    sub,
+  };
+  return Calc;   
+};
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => Object[type](object);
 
 module.exports = { calculator, arrayGenerator };
