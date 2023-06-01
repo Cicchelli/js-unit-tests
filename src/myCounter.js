04978793701b196f774520fa5026b56cc227852a
Stephan <cicchelli.stephan@gmail.com>
2023-06-01 17:06:49 -0300

    Req myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -18,12 +18,13 @@
 const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+     myArray.push(counter);
+    for (let counterT = 2; counterT <= 3; counterT += 1) {
+      myArray.push(counterT);
     }
   }
   return myArray;
 };
+console.log(myCounter());
 
 module.exports = myCounter;
